1838efddf903d044e6a42977e1307dba73fd58ea
kienson <154745768+kienson@users.noreply.github.com>
2024-05-26 17:50:07 +0200

    Update MissingDataFileException.java

--- src/library/MissingDataFileException.java
+++ src/library/MissingDataFileException.java
@@ -1,12 +1,18 @@
 package library;
 
+/**
+ * Custom exception to be thrown when the data file containing loans and user data is missing.
+ */
 public class MissingDataFileException extends Exception {
 
 	/**
-	 * 
+	 * Unique identifier for serialization.
 	 */
 	private static final long serialVersionUID = 7054098675674921293L;
-	
+
+	/**
+	 * Constructs a new MissingDataFileException with a default error message.
+	 */
 	public MissingDataFileException() {
 		super("The file containing loans and users data is not found");
 	}
